34aa7ae975bbd7092448b0968182060e93b5484e
TheGrimsey <TheGrimsey@users.noreply.github.com>
2022-04-14 16:56:15 +0200

    add config

--- src/main/java/net/thegrimsey/progressiveworldborder/ProgressiveWorldBorder.java
+++ src/main/java/net/thegrimsey/progressiveworldborder/ProgressiveWorldBorder.java
@@ -1,43 +1,31 @@
 package net.thegrimsey.progressiveworldborder;
 
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
-import net.minecraft.client.gui.hud.DebugHud;
-import net.minecraft.server.command.WorldBorderCommand;
-import net.minecraft.server.network.DebugInfoSender;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.MutableWorldProperties;
-import net.minecraft.world.WorldEvents;
 import net.minecraft.world.border.WorldBorder;
 import net.minecraft.world.level.ServerWorldProperties;
-import org.jetbrains.annotations.Debug;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ProgressiveWorldBorder implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("progressiveworldborder");
 
 	public static ProgressiveWorldBorderConfig CONFIG;
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		CONFIG = new ProgressiveWorldBorderConfig();
-
-		LOGGER.info("Hello Fabric world!");
+		// Register config file.
+		AutoConfig.register(ProgressiveWorldBorderConfig.class, JanksonConfigSerializer::new);
+		// Get config.
+		CONFIG = AutoConfig.getConfigHolder(ProgressiveWorldBorderConfig.class).getConfig();
 
 		ServerWorldEvents.LOAD.register((server, world) -> {
 			LOGGER.info("World Bordering...");
 			long day = world.getTimeOfDay() / 24000L;
-
-			LOGGER.info("Day: " + day);
 			UpdateWorldBorder(world, day, true);
 		});
 	}
@@ -53,7 +41,7 @@ public class ProgressiveWorldBorder implements ModInitializer {
 		if(skipTime)
 			worldBorder.setSize(getBorderForDay(day));
 		else
-			worldBorder.interpolateSize(worldBorder.getSize(), getBorderForDay(day), CONFIG.expansionTimeSeconds * 1000L);
+			worldBorder.interpolateSize(worldBorder.getSize(), getBorderForDay(day), (long) (CONFIG.expansionTimeSeconds * 1000L));
 
 		((ServerWorldProperties)world.getLevelProperties()).setWorldBorder(worldBorder.write());
 	}
--- src/main/java/net/thegrimsey/progressiveworldborder/ProgressiveWorldBorderConfig.java
+++ src/main/java/net/thegrimsey/progressiveworldborder/ProgressiveWorldBorderConfig.java
@@ -1,8 +1,16 @@
 package net.thegrimsey.progressiveworldborder;
 
-public class ProgressiveWorldBorderConfig {
-    public double expansionPerDay = 1.0D;
-    public double baseSize = 250.0D;
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
-    public long expansionTimeSeconds = 15L;
+@Config(name = "progressiveworldborder")
+public class ProgressiveWorldBorderConfig implements ConfigData {
+    @Comment("Amount of blocks to expand the border with each minecraft day")
+    public double expansionPerDay = 1.0D;
+    @Comment("Amount of time it takes to expand the border each day.")
+    public double expansionTimeSeconds = 5L;
+
+    @Comment("Base border size. Day amount is added onto this value.")
+    public double baseSize = 250.0D;
 }
